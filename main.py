c3fa7fab5f92eb3bbe05fcb044942047e081d3c5
jacobspaeth1 <73901547+jacobspaeth1@users.noreply.github.com>
2020-11-15 16:08:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ Period/Core:
 
 
 """
+my_string = "Hello"
+print(my_string)
+my_string = "How are you"
+print(my_string)
+("Hello " + "how are you")
+name = input("What is your name? ")
+num = int(input("how old is your brother? "))
+print(f"Hello {name}!\nMy brother is 16 and yours is {num}!")
